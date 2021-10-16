01d593c5c1d72a48cfbdc62dc1c20ef822df1020
martafontb <67740513+martafontb@users.noreply.github.com>
2021-10-16 19:33:14 +0200

    smooth scroll to top

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,14 +1,14 @@
 AOS.init();
 
-document.addEventListener("DOMContentLoaded", function(event) { 
-  //do work
-  window.scrollTo(0, 0);
-});
 
-// Scroll certain amounts from current position 
-window.scrollBy({ 
-  top: 0, // could be negative value
-  left: 0, 
-  behavior: 'smooth' 
+const scrollToTop = document.querySelector("div.back-to-top");
+
+scrollToTop.addEventListener("click", function(){
+  // Scroll certain amounts from current position 
+  window.scrollTo({ 
+    top: 0, // could be negative value
+    left: 0, 
+    behavior: 'smooth' 
+  });
 });
 
